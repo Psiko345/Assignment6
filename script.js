a61f1b583e5e1e2bb181ad1a31f648bc5fa90ace
Joshua Piper <joshua.g.piper@gmail.com>
2020-06-27 19:12:24 +1000

    created click and keypress listeners on search box. Also implemented city name change.

---
+++ script.js
@@ -0,0 +1,58 @@
+//link weather api
+let apiKey = "9b35e8a2fe8231a262c8c2468f7793c7";
+
+function getWeather(cityName) {
+  let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    return response;
+  });
+}
+window.onload = function () {
+  getWeather("sydney");
+};
+
+//click listener on search button
+$("#searchBtn").on("click", function () {
+  console.log($("#citySearchBox").val());
+  let cityName = $("#citySearchBox").val();
+  getWeather(cityName);
+  handleCurrentCityNameChange();
+});
+//buttonpress listener on search bar
+$("#citySearchBox").on("keypress", function (e) {
+  if (e.which == 13) {
+    console.log($("#citySearchBox").val());
+    let cityName = $("#citySearchBox").val();
+    getWeather(cityName);
+    handleCurrentCityNameChange();
+  }
+});
+//pull text from search bar - place in getWeather
+/* let cityName = $("#citySearchBox").val(); getWeather(cityName);*/
+
+//send cityName to proper html ID and append
+function handleCurrentCityNameChange() {
+  let cityName = $("#citySearchBox").val();
+  $("#currentCityName").text(cityName);
+  console.log($("#currentCityName").val());
+  return;
+}
+//send weather to HTML
+
+// Temperature (with celcius convertion)
+
+// Humidity
+
+// Wind Speed
+
+// Generate 5-day forcast
+//create HTML with jquery
+//ajax call the forecast using same city name
+//send data
+
+//save seach to localstorage, prepend last name to list of preivous
